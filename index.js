ad663a3d5aafd5030968c92993c7e46fdecfb8a3
Stan <s2000ProMax@gmail.com>
2022-02-06 15:08:54 +0300

    init:

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 // commit init: initialisation
+// Please change branches for look other solution
+// master - current branch
+// array - Array's solutions. Methods of arrays look like as forEach, map, find and other
 console.log('Hello world');
